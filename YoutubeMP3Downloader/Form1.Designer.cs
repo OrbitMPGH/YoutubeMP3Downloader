338c54a8c62470d63a8cd38e8631217c1433f23e
Orbit <OrbitMPGH@users.noreply.github.com>
2018-10-04 00:22:23 +0200

    Initial commit

---
+++ YoutubeMP3Downloader/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace YoutubeMP3Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox0 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.procent = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.currentlyDownloading = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textbox0
+            // 
+            this.textbox0.AutoSize = true;
+            this.textbox0.Location = new System.Drawing.Point(75, 9);
+            this.textbox0.Name = "textbox0";
+            this.textbox0.Size = new System.Drawing.Size(119, 13);
+            this.textbox0.TabIndex = 0;
+            this.textbox0.Text = "Currently Downloading: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 102);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // procent
+            // 
+            this.procent.AutoSize = true;
+            this.procent.Location = new System.Drawing.Point(125, 107);
+            this.procent.Name = "procent";
+            this.procent.Size = new System.Drawing.Size(21, 13);
+            this.procent.TabIndex = 2;
+            this.procent.Text = "0%";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Youtube Link";
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(173, 184);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(75, 23);
+            this.DownloadButton.TabIndex = 5;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // currentlyDownloading
+            // 
+            this.currentlyDownloading.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.currentlyDownloading.Location = new System.Drawing.Point(12, 43);
+            this.currentlyDownloading.Margin = new System.Windows.Forms.Padding(0);
+            this.currentlyDownloading.Name = "currentlyDownloading";
+            this.currentlyDownloading.ReadOnly = true;
+            this.currentlyDownloading.Size = new System.Drawing.Size(260, 20);
+            this.currentlyDownloading.TabIndex = 6;
+            this.currentlyDownloading.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Status: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Waiting...";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.currentlyDownloading);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.procent);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textbox0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textbox0;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label procent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.TextBox currentlyDownloading;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ YoutubeMP3Downloader/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+using NAudio.Wave;
+
+namespace YoutubeMP3Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            Download(textBox1.Text);
+        }
+
+        private void Download(string link)
+        {
+            int index = link.IndexOf('=');
+            string id = link.Remove(0, index+1);
+            label3.Text = "Loading...";
+            IEnumerable<VideoInfo> videoinfos = DownloadUrlResolver.GetDownloadUrls("https://www.youtube.com/watch?v=" + id);
+            VideoInfo video = videoinfos.First();
+
+            if (video.RequiresDecryption)
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            VideoDownloader videoDownloader;
+            String name;
+            if (String.IsNullOrWhiteSpace(textBox2.Text))
+                name = video.Title;
+            else
+                name = textBox2.Text;
+            videoDownloader = new VideoDownloader(video, Path.Combine(Application.StartupPath, name + video.VideoExtension));
+            currentlyDownloading.Text = video.Title;
+            label3.Text = "Downloading...";
+            videoDownloader.DownloadProgressChanged += (sender, args) => barchange(sender, args);
+            videoDownloader.DownloadFinished += (sender, args) => Convert(Path.Combine(Application.StartupPath, name + video.VideoExtension), name);
+            Thread download = new Thread(() => { videoDownloader.Execute(); }) { IsBackground = true };
+            download.Start();
+        }
+        delegate void StringArgReturningVoidDelegate(string text);
+
+        private void SetText(string text)
+        {
+            if (this.label3.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label3.Text = text;
+            }
+        }
+
+        private void Convert(string path, string name)
+        {
+            this.SetText("Converting...");
+            using (var reader = new MediaFoundationReader(path))
+            {
+                MediaFoundationEncoder.EncodeToMp3(reader, Path.Combine(Application.StartupPath, name + ".mp3"));
+            }
+            File.Delete(path);
+            this.SetText("Done!");
+        }
+
+        private void barchange(object sender, ProgressEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar1.Value = (int)e.ProgressPercentage;
+                procent.Text = (int)e.ProgressPercentage + "%";
+            }));
+        }
+    }
+}
